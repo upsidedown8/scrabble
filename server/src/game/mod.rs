f3695decc55371f1efa398ca6192abbf30312637
Tom Thorogood <tomthorogood@outlook.com>
2021-12-19 19:16:31 +0000

    Added module declarations

--- server/src/game/mod.rs
+++ server/src/game/mod.rs
@@ -1,6 +1,51 @@
 //! Module for handling game logic.
 
+use std::{
+    io::{self, BufRead, BufReader},
+    path::Path,
+};
+
+use {board::Board, rack::Rack, word_tree::WordTree};
+
 pub mod bitboard;
 pub mod board;
 pub mod play;
+pub mod rack;
+pub mod tile;
 pub mod word_tree;
+
+pub struct Player(String);
+
+pub enum GameStatus {
+    Winner(Player),
+    ToPlay(Player),
+}
+
+pub struct Game {
+    tree: WordTree,
+    racks: Vec<Rack>,
+    board: Board,
+}
+
+impl Game {
+    pub fn new<P>(word_file: P) -> io::Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        // read words from file and populate tree
+        let mut tree = WordTree::default();
+
+        let file = std::fs::File::open(word_file)?;
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            tree.insert(line?.trim());
+        }
+
+        Ok(Self {
+            tree,
+            board: Board::default(),
+            racks: vec![],
+        })
+    }
+}
