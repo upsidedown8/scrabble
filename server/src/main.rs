a5b84c6526e7d55e1d36556cee3402e5be88678b
Tom Thorogood <tomthorogood@outlook.com>
2021-12-19 19:16:53 +0000

    Added basic example to `main.rs`

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,60 +1,91 @@
-#[macro_use]
-extern crate rocket;
+use server::game::{
+    board::{Board, Direction, Pos},
+    play::{Play, Word},
+    tile::{Letter, Tile},
+    word_tree::*,
+};
 
-use std::env;
+// #[macro_use]
+// extern crate rocket;
 
-use anyhow::Result;
+// use std::env;
 
-use rocket::{http::Status, State};
+// use anyhow::Result;
 
-use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
+// use rocket::{http::Status, State};
 
-#[derive(Debug)]
-pub struct User {
-    pub id_user: i64,
-    pub username: String,
-    pub password: String,
-    pub salt: String,
-}
+// use sqlx::sqlite::{SqlitePool, SqlitePoolOptions};
 
-impl User {
-    pub async fn find_by_id(id: i32, pool: &SqlitePool) -> Result<Self> {
-        let user = sqlx::query_as!(User, "SELECT * FROM tbl_user WHERE id_user = ?", id)
-            .fetch_one(&*pool)
-            .await?;
+// #[derive(Debug)]
+// pub struct User {
+//     pub id_user: i64,
+//     pub username: String,
+//     pub password: String,
+//     pub salt: String,
+// }
 
-        Ok(user)
-    }
-}
+// impl User {
+//     pub async fn find_by_id(id: i32, pool: &SqlitePool) -> Result<Self> {
+//         let user = sqlx::query_as!(User, "SELECT * FROM tbl_user WHERE id_user = ?", id)
+//             .fetch_one(&*pool)
+//             .await?;
 
-#[get("/user/<id>")]
-async fn user(pool: &State<SqlitePool>, id: i32) -> Result<String, Status> {
-    let user = User::find_by_id(id, pool).await;
+//         Ok(user)
+//     }
+// }
 
-    match user {
-        Ok(user) => Ok(format!("Hi {}", user.username)),
-        _ => Err(Status::NotFound),
-    }
-}
+// #[get("/user/<id>")]
+// async fn user(pool: &State<SqlitePool>, id: i32) -> Result<String, Status> {
+//     let user = User::find_by_id(id, pool).await;
 
-#[rocket::main]
-async fn main() -> Result<()> {
-    dotenv::dotenv().expect("A `.env` file to be present in the working directory");
+//     match user {
+//         Ok(user) => Ok(format!("Hi {}", user.username)),
+//         _ => Err(Status::NotFound),
+//     }
+// }
 
-    let db_url = env::var("DATABASE_URL")?;
+// #[rocket::main]
+// async fn main() -> Result<()> {
+// dotenv::dotenv().expect("A `.env` file to be present in the working directory");
 
-    println!("url: {}", db_url);
+// let db_url = env::var("DATABASE_URL")?;
 
-    let pool = SqlitePoolOptions::new()
-        .max_connections(5)
-        .connect(&db_url)
-        .await?;
+// println!("url: {}", db_url);
 
-    rocket::build()
-        .mount("/", routes![user])
-        .manage(pool)
-        .launch()
-        .await?;
+// let pool = SqlitePoolOptions::new()
+//     .max_connections(5)
+//     .connect(&db_url)
+//     .await?;
 
-    Ok(())
+// rocket::build()
+//     .mount("/", routes![user])
+//     .manage(pool)
+//     .launch()
+//     .await?;
+
+//     Ok(())
+// }
+
+fn main() {
+    let mut board = Board::default();
+
+    let w = vec![
+        Word::new("apple", Direction::Down, (5, 3)).unwrap(),
+        Word::new("please", Direction::Right, (6, 2)).unwrap(),
+    ];
+    let play = Play::from_words(w.into_iter());
+
+    println!("{}", play.occupancy());
+
+    board.make_play(play);
+
+    let t = vec![
+        (Pos::from((10, 10)), Tile::Letter(Letter::new('a').unwrap())),
+        (Pos::from((11, 11)), Tile::Blank(Letter::new('a'))),
+        (Pos::from((12, 12)), Tile::Blank(None)),
+    ];
+
+    board.make_play(Play::from_tiles(t.into_iter()));
+
+    println!("{}", board);
 }
