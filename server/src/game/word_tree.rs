33c1d7bfd280448c241d88710f2abe92dc7278a8
Tom Thorogood <tomthorogood@outlook.com>
2021-12-19 19:15:23 +0000

    Fully iplemented `WordTree` with tests

--- server/src/game/word_tree.rs
+++ server/src/game/word_tree.rs
@@ -1,78 +1,123 @@
 //! Module containing a word tree
 
+use super::tile::Letter;
+
+#[derive(Clone, Copy, Debug)]
+pub struct NodeIndex(usize);
+
+#[derive(Default, Debug)]
 pub struct Node {
-    pub is_terminal: bool,
-    pub children: [Option<usize>; 26],
+    is_terminal: bool,
+    children: [Option<NodeIndex>; 26],
 }
-
 impl Node {
-    pub fn get_child(&self, ch: usize) -> Option<usize> {
-        self.children[ch]
+    pub fn set_child(&mut self, letter: Letter, idx: NodeIndex) {
+        self.children[usize::from(letter)] = Some(idx);
+    }
+    pub fn get_child(&self, letter: Letter) -> Option<NodeIndex> {
+        self.children[usize::from(letter)]
+    }
+    pub fn children(&self) -> impl Iterator<Item = &Option<NodeIndex>> {
+        self.children.iter()
+    }
+    pub fn is_terminal(&self) -> bool {
+        self.is_terminal
+    }
+    pub fn set_terminal(&mut self, is_terminal: bool) {
+        self.is_terminal = is_terminal;
+    }
+    pub fn new(is_terminal: bool) -> Self {
+        Self {
+            is_terminal,
+            children: Default::default(),
+        }
     }
 }
 
+#[derive(Debug)]
 pub struct WordTree {
-    init: [Option<usize>; 26],
+    root: usize,
     nodes: Vec<Node>,
-    count: usize,
 }
 
+impl Default for WordTree {
+    fn default() -> Self {
+        Self {
+            root: 0,
+            nodes: vec![Node::new(false)],
+        }
+    }
+}
 impl WordTree {
-    pub fn str_to_iter(word: &str) -> impl Iterator<Item = usize> + '_ {
-        word.chars().filter_map(|ch| match ch {
-            'a'..='z' => Some((ch as usize) - 97),
-            'A'..='Z' => Some((ch as usize) - 65),
-            _ => None,
-        })
+    pub fn root_idx(&self) -> NodeIndex {
+        NodeIndex(self.root)
     }
-    pub fn len(&self) -> usize {
-        self.count
+
+    pub fn node(&self, idx: NodeIndex) -> &Node {
+        &self.nodes[idx.0]
     }
-    pub fn is_empty(&self) -> bool {
-        self.count == 0
+    pub fn node_mut(&mut self, idx: NodeIndex) -> &mut Node {
+        &mut self.nodes[idx.0]
+    }
+
+    pub fn trace_word(&self, root: NodeIndex, word: &str) -> Option<NodeIndex> {
+        let mut curr_idx = root;
+
+        // follow a path along the tree defined by the word.
+        for letter in word.chars().filter_map(Letter::new) {
+            curr_idx = self.node(curr_idx).get_child(letter)?;
+        }
+
+        Some(curr_idx)
     }
     pub fn insert(&mut self, word: &str) {
-        let mut children = &self.init;
-        let mut iter = Self::str_to_iter(word).peekable();
+        let mut curr_idx = self.root_idx();
 
-        while let Some(ch) = iter.next() {
-            let idx = match children[ch] {
+        for letter in word.chars().filter_map(Letter::new) {
+            let idx = match self.node(curr_idx).get_child(letter) {
                 Some(idx) => idx,
                 None => {
-                    let idx = self.nodes.len();
-                    self.nodes.push(Node {
-                        is_terminal: iter.peek().is_none(),
-                        children: [None; 26],
-                    });
-                    idx
+                    self.nodes.push(Node::default());
+                    NodeIndex(self.nodes.len() - 1)
                 }
             };
 
-            children = &self.nodes[idx].children;
+            // add node at `idx` as a child of the node at`curr_idx`
+            self.node_mut(curr_idx).set_child(letter, idx);
+
+            // update current node
+            curr_idx = idx;
         }
+
+        self.node_mut(curr_idx).set_terminal(true);
     }
     pub fn contains(&self, word: &str) -> bool {
-        let mut children = &self.init;
-        let mut iter = Self::str_to_iter(word).peekable();
-
-        // follow a path along the tree defined by the word.
-        while let Some(ch) = iter.next() {
-            let idx = match children[ch] {
-                // if there is a child node, and we have reached the end of the
-                // string, then if the node is terminal, the word is contained,
-                // otherwise the word is not contained.
-                Some(idx) if iter.peek().is_none() => return self.nodes[idx].is_terminal,
-                // otherwise return the index of the next child node
-                Some(idx) => idx,
-                // if there is no child node then the item is not in the list.
-                None => break,
-            };
-
-            // update the children reference
-            children = &self.nodes[idx].children;
+        match self.trace_word(self.root_idx(), word) {
+            Some(idx) => self.node(idx).is_terminal(),
+            None => false,
         }
+    }
+}
 
-        // otherwise must be false
-        false
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn insert_contains() {
+        let mut word_tree = WordTree::default();
+
+        word_tree.insert("abade");
+        word_tree.insert("abide");
+        word_tree.insert("a");
+        word_tree.insert("collection");
+        word_tree.insert("collect");
+
+        assert!(!word_tree.contains("death"));
+        assert!(!word_tree.contains("collecti"));
+        assert!(word_tree.contains("collection"));
+        assert!(word_tree.contains("abide"));
+        assert!(word_tree.contains("abade"));
+        assert!(!word_tree.contains("abadf"));
     }
 }
