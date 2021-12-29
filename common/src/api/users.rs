7f2cee91ec48dcb31891bb8403bda8dce97da95d
Tom Thorogood <tomthorogood@outlook.com>
2021-12-29 18:18:42 +0000

    API types for `/users`

---
+++ common/src/api/users.rs
@@ -0,0 +1,94 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Auth {
+    pub token: String,
+}
+
+/**--------------------------------------------
+ *               Utils
+ *---------------------------------------------**/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserDetails {
+    pub username: String,
+    pub email: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserProfile {
+    pub username: String,
+}
+
+/**--------------------------------------------
+ *               Login route
+ *---------------------------------------------**/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserLogin {
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserLoginResponse {
+    pub auth: Auth,
+    pub user_details: UserDetails,
+}
+
+/**--------------------------------------------
+ *               Create user route
+ *---------------------------------------------**/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserCreate {
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserCreateResponse {
+    pub auth: Auth,
+    pub user_details: UserDetails,
+}
+
+/**--------------------------------------------
+ *               Get user details route
+ *---------------------------------------------**/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserInfo {
+    pub auth: Auth,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserInfoResponse {
+    pub user_details: UserDetails,
+}
+
+/**--------------------------------------------
+ *               Update user info route
+ *---------------------------------------------**/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserUpdate {
+    pub auth: Auth,
+    pub old_password: String,
+    pub email: Option<String>,
+    pub username: Option<String>,
+    pub password: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserUpdateResponse {
+    pub user_details: UserDetails,
+}
+
+/**--------------------------------------------
+ *               Delete user route
+ *---------------------------------------------**/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeleteUser {
+    pub auth: Auth,
+    pub password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct DeleteUserResponse {
+    pub user_details: UserDetails,
+}
