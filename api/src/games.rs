81e02f8b4f0e338efc8fdb74992076d1357e2f83
Tom Thorogood <tomthorogood@outlook.com>
2022-02-12 15:43:36 +0000

    Add `games` module

---
+++ api/src/games.rs
@@ -0,0 +1,9 @@
+// use uuid::Uuid;
+
+// pub struct GameCreate {
+
+// }
+
+// pub struct GameCreateResponse {
+//     pub game_id: Uuid,
+// }
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,3 +2,4 @@
 //! to communicate.
 
 pub mod users;
+pub mod games;
