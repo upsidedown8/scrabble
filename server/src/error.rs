ccee9eeae6ad7f87d89c45d4a2c9c025ca2d7cdd
Tom Thorogood <tomthorogood@outlook.com>
2022-02-12 22:02:17 +0000

    Add `error` module

--- server/src/error.rs
+++ server/src/error.rs
@@ -2,11 +2,32 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 impl warp::reject::Reject for Error {}
 
-#[derive(Clone, Debug)]
+impl From<sqlx::Error> for Error {
+    fn from(err: sqlx::Error) -> Self {
+        Self::SqlxError(err)
+    }
+}
+impl From<uuid::Error> for Error {
+    fn from(err: uuid::Error) -> Self {
+        Self::UuidError(err)
+    }
+}
+impl From<argon2::Error> for Error {
+    fn from(err: argon2::Error) -> Self {
+        Self::Argon2Error(err)
+    }
+}
+
+#[derive(Debug)]
 pub enum Error {
+    SqlxError(sqlx::Error),
+    UuidError(uuid::Error),
+    Argon2Error(argon2::Error),
+    UsernameExistsError,
     InsufficientRoleError,
     MissingAuthHeaderError,
     InvalidAuthHeaderError,
+    IncorrectPasswordError,
     JwtEncodingError(jsonwebtoken::errors::Error),
     JwtDecodingError(jsonwebtoken::errors::Error),
 }
