37c012c8eb58793ca6a7158cde734a2f087bc43e
Tom Thorogood <tomthorogood@outlook.com>
2022-02-13 21:49:20 +0000

    More consistent scripts

--- server/scripts/install_deps.sh
+++ server/scripts/install_deps.sh
@@ -1,8 +1,6 @@
-echo "install sqlx"
+echo ":: install sqlx"
 cargo install sqlx
 
-echo "install openssl"
-pacman -S openssl
+echo ":: install openssl, docker"
+pacman -S openssl docker
 
-echo "install docker"
-# pacman -S docker
--- server/scripts/setup.sh
+++ server/scripts/setup.sh
@@ -1,16 +1,18 @@
-echo "create directories"
+echo ":: create data directories"
 mkdir -p data/db
 mkdir -p data/cert
 
-echo "run sqlx migrate"
-sqlx migrate run
+echo ":: run sqlx database setup"
+sqlx database setup --database-url 'sqlite://data/db/scrabble.db'
 
-echo "generate certificate"
+echo ":: generate certificate"
 openssl req -newkey rsa:2048 -new -nodes -x509 \
     -days 3650 \
     -keyout data/cert/key.rsa \
     -out data/cert/cert.pem \
     -subj "/C=GB/ST=West-Sussex/L=Worthing/O=Scrabble AI"
 
-echo -n "jwt secret: "
+echo ":: generating jwt secret"
+echo -n "  jwt secret: "
 openssl rand -hex 64
+
