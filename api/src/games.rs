c4909a6bdcfbcb05623b136acb02c3a25a65c7c0
Tom Thorogood <tomthorogood@outlook.com>
2022-02-13 00:41:08 +0000

    Add `GameMessage` enum

--- api/src/games.rs
+++ api/src/games.rs
@@ -7,3 +7,10 @@
 // pub struct GameCreateResponse {
 //     pub game_id: Uuid,
 // }
+
+// pub enum GameMessage {
+//     Chat(PlayerId, Message),
+//     Join(PlayerDetails),
+//     Leave(PlayerId),
+//     Play(Play),
+// }
