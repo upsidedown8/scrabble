ea89ccec23bd8a80d6574370fdd159325e6dab7b
Tom Thorogood <tomthorogood@outlook.com>
2022-02-13 00:44:36 +0000

    Update error typenames

--- server/src/routes/mod.rs
+++ server/src/routes/mod.rs
@@ -10,7 +10,7 @@ mod words;
 
 /// Gets a filter for all routes
 pub fn all(db: Db) -> impl Filter<Extract = (impl Reply,), Error = Infallible> + Clone {
-    let routes = users::all(db.clone());
+    let routes = users::all(db);
 
     warp::path("api").and(routes).recover(handle_rejection)
 }
@@ -19,18 +19,16 @@ pub fn all(db: Db) -> impl Filter<Extract = (impl Reply,), Error = Infallible> +
 async fn handle_rejection(rejection: Rejection) -> Result<impl Reply, Infallible> {
     let (status, msg) = if let Some(error) = rejection.find::<Error>() {
         match error {
-            Error::InsufficientRoleError => (StatusCode::UNAUTHORIZED, "Unauthorized"),
-            Error::InvalidAuthHeaderError => (StatusCode::BAD_REQUEST, "Invalid auth header"),
-            Error::MissingAuthHeaderError => (StatusCode::NOT_FOUND, "Missing auth header"),
-            Error::JwtDecodingError(_) => (StatusCode::UNAUTHORIZED, "Unauthorized"),
-            Error::JwtEncodingError(_) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
-            }
-            Error::SqlxError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-            Error::UuidError(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-            Error::Argon2Error(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-            Error::IncorrectPasswordError => (StatusCode::UNAUTHORIZED, "Unauthorized"),
-            Error::UsernameExistsError => (StatusCode::BAD_REQUEST, "Username exists"),
+            Error::InsufficientRole => (StatusCode::UNAUTHORIZED, "Unauthorized"),
+            Error::InvalidAuthHeader => (StatusCode::BAD_REQUEST, "Invalid auth header"),
+            Error::MissingAuthHeader => (StatusCode::NOT_FOUND, "Missing auth header"),
+            Error::JwtDecoding(_) => (StatusCode::UNAUTHORIZED, "Unauthorized"),
+            Error::JwtEncoding(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
+            Error::Sqlx(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
+            Error::Uuid(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
+            Error::Argon2(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
+            Error::IncorrectPassword => (StatusCode::UNAUTHORIZED, "Unauthorized"),
+            Error::UsernameExists => (StatusCode::BAD_REQUEST, "Username exists"),
         }
     } else if rejection.is_not_found() {
         (StatusCode::NOT_FOUND, "Not found")
