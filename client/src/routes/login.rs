dc1f944562c6b207db7be05f87fcd5dde239ccf4
Tom Thorogood <tomthorogood@outlook.com>
2022-02-22 17:14:15 +0000

    Implement login and sign_up routes

--- client/src/routes/login.rs
+++ client/src/routes/login.rs
@@ -1,11 +1,79 @@
-use sycamore::prelude::*;
+use crate::{
+    components::{ErrorMsg, ProgressBar},
+    contexts::{auth::AuthCtx, ScopeAuthExt},
+    services::users::login,
+};
+use api::users::Login;
+use sycamore::{futures::ScopeSpawnFuture, prelude::*};
+use sycamore_router::navigate;
 
 #[component]
 pub fn LoginPage<G: Html>(ctx: ScopeRef) -> View<G> {
+    let auth_ctx = ctx.use_auth_context();
+    let username = ctx.create_signal(String::from(""));
+    let password = ctx.create_signal(String::from(""));
+    let login_req = ctx.create_memo(|| Login {
+        username: (*username.get()).clone(),
+        password: (*password.get()).clone(),
+    });
+    let loading = ctx.create_signal(false);
+    let err = ctx.create_signal(None);
+
+    let onsignin = |_| {
+        loading.set(true);
+
+        ctx.spawn_future(async {
+            match login(auth_ctx, login_req.get().as_ref()).await {
+                Ok(login_response) => {
+                    auth_ctx.set(Some(AuthCtx {
+                        details: login_response.user_details,
+                        auth: login_response.auth,
+                    }));
+
+                    navigate("/");
+                }
+                Err(e) => err.set(Some(e)),
+            };
+
+            loading.set(false);
+        });
+    };
+
     view! { ctx,
-        div(class="login-route") {
-            h1 {
-                "Login"
+        div(class="login-route is-centered is-vcentered is-flex columns") {
+            div(class="box") {
+                div(class="has-text-centered") {
+                    a(href="/signup") {
+                        "Need an account?"
+                    }
+                }
+
+                hr
+
+                div(class="field") {
+                    label(class="label") {
+                        "Username"
+                    }
+                    div(class="control") {
+                        input(type="text", class="input", placeholder="username", bind:value=username)
+                    }
+                }
+
+                div(class="field") {
+                    label(class="label") {
+                        "Password"
+                    }
+                    div(class="control") {
+                        input(type="password", class="input", placeholder="**********", bind:value=password)
+                    }
+                }
+
+                button(on:click=onsignin, disabled=*loading.get(), class="button is-primary") {
+                    "Sign in"
+                }
+
+                ProgressBar(loading)
+                ErrorMsg(err)
             }
         }
     }
--- client/src/routes/sign_up.rs
+++ client/src/routes/sign_up.rs
@@ -1,11 +1,90 @@
-use sycamore::prelude::*;
+use crate::{
+    components::{ErrorMsg, ProgressBar},
+    contexts::{auth::AuthCtx, ScopeAuthExt},
+    services::users::sign_up,
+};
+use api::users::SignUp;
+use sycamore::{futures::ScopeSpawnFuture, prelude::*};
+use sycamore_router::navigate;
 
 #[component]
 pub fn SignUpPage<G: Html>(ctx: ScopeRef) -> View<G> {
+    let auth_ctx = ctx.use_auth_context();
+    let username = ctx.create_signal(String::from(""));
+    let email = ctx.create_signal(String::from(""));
+    let password = ctx.create_signal(String::from(""));
+    let sign_up_req = ctx.create_memo(|| SignUp {
+        username: (*username.get()).clone(),
+        email: (*email.get()).clone(),
+        password: (*password.get()).clone(),
+    });
+    let loading = ctx.create_signal(false);
+    let err = ctx.create_signal(None);
+
+    let onsignup = |_| {
+        loading.set(true);
+
+        ctx.spawn_future(async {
+            match sign_up(auth_ctx, sign_up_req.get().as_ref()).await {
+                Ok(sign_up_response) => {
+                    auth_ctx.set(Some(AuthCtx {
+                        details: sign_up_response.user_details,
+                        auth: sign_up_response.auth,
+                    }));
+
+                    navigate("/");
+                }
+                Err(e) => err.set(Some(e)),
+            };
+
+            loading.set(false);
+        });
+    };
+
     view! { ctx,
-        div(class="signup-route") {
-            h1 {
-                "Sign up"
+        div(class="signup-route is-centered is-vcentered is-flex columns") {
+            div(class="box") {
+                div(class="has-text-centered") {
+                    a(href="/login") {
+                        "Already have an account?"
+                    }
+                }
+
+                hr
+
+                div(class="field") {
+                    label(class="label") {
+                        "Username"
+                    }
+                    div(class="control") {
+                        input(type="text", class="input", placeholder="username", bind:value=username)
+                    }
+                }
+
+                div(class="field") {
+                    label(class="label") {
+                        "Email"
+                    }
+                    div(class="control") {
+                        input(type="email", class="input", placeholder="you@example.com", bind:value=email)
+                    }
+                }
+
+                div(class="field") {
+                    label(class="label") {
+                        "Password"
+                    }
+                    div(class="control") {
+                        input(type="password", class="input", placeholder="**********", bind:value=password)
+                    }
+                }
+
+                button(on:click=onsignup, disabled=*loading.get(), class="button is-primary") {
+                    "Sign up"
+                }
+
+                ProgressBar(loading)
+                ErrorMsg(err)
             }
         }
     }
