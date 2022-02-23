1b4f566b5dffa417f1cd26fa7ec6bcbb504122ab
Tom Thorogood <tomthorogood@outlook.com>
2022-02-23 00:49:24 +0000

    Fix `ProgressBar` updates

--- client/src/components/progress_bar.rs
+++ client/src/components/progress_bar.rs
@@ -1,13 +1,15 @@
 use sycamore::prelude::*;
 
 #[component]
-pub fn ProgressBar<'a, G: Html>(ctx: ScopeRef<'a>, visible: &'a ReadSignal<bool>) -> View<G> {
-    match *visible.get() {
-        false => view! { ctx, },
-        true => view! { ctx,
-            progress(class="progress is-small is-primary mt-3") {
-                "10%"
-            }
-        },
+pub fn ProgressBar<'a, G: Html>(ctx: ScopeRef<'a>, visible: &'a Signal<bool>) -> View<G> {
+    view! { ctx,
+        (match *visible.get() {
+            false => view! { ctx, },
+            true => view! { ctx,
+                progress(class="progress is-small is-primary mt-3") {
+                    "10%"
+                }
+            },
+        })
     }
 }
