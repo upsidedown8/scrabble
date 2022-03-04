4f1c96f0e93b19b88812a5eb2d3634ca2ff1378f
Tom Thorogood <tomthorogood@outlook.com>
2022-03-04 22:45:09 +0000

    Add `Ai` trait

--- scrabble/src/ai/mod.rs
+++ scrabble/src/ai/mod.rs
@@ -1,3 +1,11 @@
 //! Scrabble AI implementation.
 
+use crate::game::{play::Play, board::Board};
+
 pub mod movegen;
+
+/// Serves as a common interface over AI implementations.
+pub trait Ai {
+    /// Finds the best move from a given position.
+    fn best_move(board: &Board) -> Play;
+}
