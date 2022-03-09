5fe40ac3a8f1a9c5b62d31c563802dfeba3d28c5
Tom Thorogood <tomthorogood@outlook.com>
2022-03-09 00:43:02 +0000

    Implement vertical move generation

--- scrabble/src/ai/movegen.rs
+++ scrabble/src/ai/movegen.rs
@@ -11,59 +11,86 @@ use crate::{
 };
 use std::{collections::HashMap, iter::successors};
 
-/// An iterator over the moves for a board position.
-pub struct Moves<'a, F> {
-    fsm: &'a F,
-    board: &'a Board,
-    rack: &'a Rack,
+/// Gets a list containing all moves that satisfy the filter function
+/// for the board position.
+pub fn gen<'a>(board: &Board, rack: &Rack, fsm: &'a impl Fsm<'a>) -> Vec<(Play, usize)> {
+    let mut plays = vec![];
+
+    let mut h_moves = MoveGen::new(rack, *board.occ_h(), fsm, |pos| board.get(pos), |pos| pos);
+    h_moves.push_moves(&mut plays);
+
+    let mut v_moves = MoveGen::new(
+        rack,
+        *board.occ_v(),
+        fsm,
+        |pos| board.get(pos.anti_clockwise90()),
+        |pos| pos.clockwise90(),
+    );
+    v_moves.push_moves(&mut plays);
+
+    plays
 }
 
-impl<'a, F: Fsm<'a>> Moves<'a, F> {
-    /// Creates a new iterator over the horizontal moves on the board.
-    pub fn new(fsm: &'a F, board: &'a Board, rack: &'a Rack) -> Self {
-        Self { fsm, board, rack }
+/// Stores state shared across calls to `push_moves_recursive`,
+/// so that fewer arguments need to be passed to the function. The generic
+/// parameters `G` and `H` are functions that map a position to a board
+/// tile, and a transformed position (horizontal) back to the actual position.
+struct MoveGen<'a, F, G, H> {
+    fsm: &'a F,
+    get_cell: G,
+    map_pos: H,
+    lookup: Vec<HashMap<Tile, usize>>,
+
+    occ: BitBoard,
+    above_or_below: BitBoard,
+    illegal_ends: BitBoard,
+
+    stack: Vec<(Pos, Tile)>,
+    counts: TileCounts,
+}
+
+impl<'a, F, G, H> MoveGen<'a, F, G, H>
+where
+    F: Fsm<'a>,
+    G: Fn(Pos) -> Option<Tile>,
+    H: Fn(Pos) -> Pos,
+{
+    /// Creates a new [`MoveGen`].
+    pub fn new(rack: &Rack, occ: BitBoard, fsm: &'a F, get_cell: G, map_pos: H) -> Self {
+        let above_or_below = occ.above_or_below();
+        let illegal_ends = occ.west();
+        let lookup: Vec<_> = (0..225).map(|_| HashMap::new()).collect();
+
+        let mut move_gen = Self {
+            fsm,
+            get_cell,
+            map_pos,
+            lookup,
+            occ,
+            above_or_below,
+            illegal_ends,
+            stack: vec![],
+            counts: *rack.tile_counts(),
+        };
+
+        // init the `lookup` table.
+        move_gen.init_lookup_v();
+
+        move_gen
     }
 
     /// Returns the list of horizontal moves for a position.
-    pub fn moves(&self) -> Vec<(Play, usize)> {
-        let &occ_h = self.board.occ_h();
-        let above_or_below = occ_h.above_or_below();
-        let illegal_ends = occ_h.west();
-
-        let starts = Self::all_starts_h(*self.board.occ_h(), self.rack.len());
-        let lookup = self.lookup_v();
-        let mut plays = vec![];
-        let mut stack = vec![];
-        let mut counts = *self.rack.tile_counts();
+    pub fn push_moves(&mut self, plays: &mut Vec<(Play, usize)>) {
+        let starts = Self::all_starts_h(self.occ, self.counts.len());
 
         for start in starts {
-            self.add_moves(
-                &mut plays,
-                &lookup,
-                &mut counts,
-                &mut stack,
-                &above_or_below,
-                &illegal_ends,
-                Some(start),
-                self.fsm.initial_state(),
-                0,
-                1,
-                false,
-            );
+            self.push_moves_recursive(plays, Some(start), self.fsm.initial_state(), 0, 1, false);
         }
-
-        plays
     }
     /// Recursively traverses possible moves and adds them to the list.
-    #[allow(clippy::too_many_arguments)]
-    fn add_moves(
-        &self,
+    fn push_moves_recursive(
+        &mut self,
         plays: &mut Vec<(Play, usize)>,
-        lookup: &[HashMap<Tile, usize>],
-        counts: &mut TileCounts,
-        stack: &mut Vec<(Pos, Tile)>,
-        above_or_below: &BitBoard,
-        illegal_ends: &BitBoard,
         pos: Option<Pos>,
         state: StateId,
         score: usize,
@@ -73,9 +100,25 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
         if connected && self.fsm.is_terminal(state) {
             // check that the final stack item does not have
             // a disallowed end position & that stack is not empty.
-            if let Some(&(pos, _)) = stack.last() {
-                if !illegal_ends.is_bit_set(pos) {
-                    plays.push((Play::Place(stack.clone()), score * multiplier));
+            if let Some(&(pos, _)) = self.stack.last() {
+                // prevents illegal plays if the end of a word is adjacent to a tile.
+                if !self.illegal_ends.is_bit_set(pos) {
+                    // prevents doubled up moves from horizontal and vertical generation.
+                    // if there is only one tile, it must not be adjacent.
+                    if self.stack.len() > 1 || !self.above_or_below.is_bit_set(pos) {
+                        plays.push((
+                            Play::Place(
+                                self.stack
+                                    .iter()
+                                    .copied()
+                                    // run `map_pos` on each position to convert transposed coordinates
+                                    // back to their actual values.
+                                    .map(|(pos, tile)| ((self.map_pos)(pos), tile))
+                                    .collect(),
+                            ),
+                            score * multiplier,
+                        ));
+                    }
                 }
             }
         }
@@ -83,25 +126,15 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
         if let Some(pos) = pos {
             let next_pos = pos.dir(Direction::East);
 
-            match self.board.at(pos) {
+            match (self.get_cell)(pos) {
                 // Already a tile: see whether traversal can continue (no branching).
                 // Add the tile score to the total score.
                 Some(tile @ Tile::Letter(letter) | tile @ Tile::Blank(Some(letter))) => {
                     let score = score + tile.score();
 
                     if let Some(next_state) = self.fsm.traverse_from(state, letter) {
-                        self.add_moves(
-                            plays,
-                            lookup,
-                            counts,
-                            stack,
-                            above_or_below,
-                            illegal_ends,
-                            next_pos,
-                            next_state,
-                            score,
-                            multiplier,
-                            true,
+                        self.push_moves_recursive(
+                            plays, next_pos, next_state, score, multiplier, true,
                         );
                     }
                 }
@@ -110,40 +143,35 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
                 // words are also valid).
                 _ => {
                     let (tile_m, word_m) = pos.premium_multipliers();
-                    let has_adjacant = above_or_below.is_bit_set(pos);
+                    let has_adjacent = self.above_or_below.is_bit_set(pos);
 
                     // try each transition from this state.
                     for (letter, next_state) in self.fsm.transitions(state) {
                         for tile in [Tile::Letter(letter), Tile::Blank(Some(letter))] {
-                            let extra_score = match has_adjacant {
+                            let extra_score = match has_adjacent {
                                 false => 0,
-                                true => match lookup[usize::from(pos)].get(&tile) {
+                                true => match self.lookup[usize::from(pos)].get(&tile) {
                                     Some(&score) => score,
                                     // not a valid tile, continue to next loop.
                                     _ => continue,
                                 },
                             };
 
-                            if counts.any(tile) {
-                                counts.remove_one(letter);
-                                stack.push((pos, tile));
+                            if self.counts.any(tile) {
+                                self.counts.remove_one(letter);
+                                self.stack.push((pos, tile));
 
-                                self.add_moves(
+                                self.push_moves_recursive(
                                     plays,
-                                    lookup,
-                                    counts,
-                                    stack,
-                                    above_or_below,
-                                    illegal_ends,
                                     next_pos,
                                     next_state,
                                     extra_score + score + tile_m * tile.score(),
                                     multiplier * word_m,
-                                    connected || has_adjacant,
+                                    connected || has_adjacent,
                                 );
 
-                                stack.pop();
-                                counts.insert_one(letter);
+                                self.stack.pop();
+                                self.counts.insert_one(letter);
                             }
                         }
                     }
@@ -177,11 +205,7 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
     /// always occurs in a single row, only 1 tile can be placed in each
     /// column. This function produces a hashmap of legal tiles and their
     /// corresponding scores for each position on the board.
-    fn lookup_v(&self) -> Vec<HashMap<Tile, usize>> {
-        let counts = self.rack.tile_counts();
-        let mut lookup: Vec<_> = (0..225).map(|_| HashMap::new()).collect();
-        let above_or_below = self.board.occ_h().above_or_below();
-
+    fn init_lookup_v(&mut self) {
         // Each column can be considered seperately. Considering
         // columns seperately also means that fewer fsm traversals
         // are required, as these words are vertical.
@@ -191,7 +215,7 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
 
             // Go down the rows for the current column
             for pos in Row::iter().map(|row| Pos::from((row, col))) {
-                match self.board.at(pos) {
+                match (self.get_cell)(pos) {
                     // already a tile at this position, update score and state.
                     Some(tile @ Tile::Letter(letter) | tile @ Tile::Blank(Some(letter))) => {
                         // since the tile has already been placed, the path should be in the fsm.
@@ -203,15 +227,15 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
                         // if the position is not directly above or below
                         // an existing square then words placed there ignore
                         // the map for that square.
-                        if above_or_below.is_bit_set(pos) {
+                        if self.above_or_below.is_bit_set(pos) {
                             // try all tiles that could be placed here.
                             for (letter, next_state) in self.fsm.transitions(state) {
                                 for tile in [Tile::Letter(letter), Tile::Blank(Some(letter))] {
-                                    if counts.any(tile) {
+                                    if self.counts.any(tile) {
                                         if let Some((tile, score)) =
                                             self.score_v(tile, score, pos, next_state)
                                         {
-                                            lookup[usize::from(pos)].insert(tile, score);
+                                            self.lookup[usize::from(pos)].insert(tile, score);
                                         }
                                     }
                                 }
@@ -225,11 +249,15 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
                 }
             }
         }
-
-        lookup
     }
     /// Finds the scores for a vertical word from a position.
-    fn score_v(&self, tile: Tile, score: usize, pos: Pos, state: StateId) -> Option<(Tile, usize)> {
+    fn score_v(
+        &mut self,
+        tile: Tile,
+        score: usize,
+        pos: Pos,
+        state: StateId,
+    ) -> Option<(Tile, usize)> {
         let (tile_m, multiplier) = pos.premium_multipliers();
         let mut score = score + tile_m * tile.score();
         let mut state = state;
@@ -239,7 +267,7 @@ impl<'a, F: Fsm<'a>> Moves<'a, F> {
         //  - the end of the board is encountered
         // skip `pos` as it was previously validated and scored.
         for pos in successors(pos.dir(Direction::South), |p| p.dir(Direction::South)) {
-            match self.board.at(pos) {
+            match (self.get_cell)(pos) {
                 Some(tile @ Tile::Letter(letter) | tile @ Tile::Blank(Some(letter))) => {
                     // these tiles are already placed so premium does not apply
                     score += tile.score();
