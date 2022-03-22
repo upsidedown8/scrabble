ec47a19c4ef02179482b88b8bf0d7b0744b2dfb7
Tom Thorogood <tomthorogood@outlook.com>
2022-03-22 18:25:26 +0000

    Update and improve error handing for `scoring::score`

--- scrabble/src/util/scoring.rs
+++ scrabble/src/util/scoring.rs
@@ -2,30 +2,23 @@
 
 use crate::{
     error::{GameError, GameResult},
-    game::board::Board,
     util::{bitboard::BitBoard, fsm::Fsm, words::Word},
 };
 
 /// Validates a word and finds its score.
-pub fn score<'a>(
-    word: Word,
-    new: &BitBoard,
-    board: &Board,
-    fsm: &impl Fsm<'a>,
-) -> GameResult<usize> {
+pub fn score<'a>(word: Word<'_>, new: &BitBoard, fsm: &impl Fsm<'a>) -> GameResult<usize> {
     let mut score = 0;
     let mut word_multiplier = 1;
     let mut curr_state = fsm.initial_state();
 
-    for pos in word {
-        let tile = board.get(pos).expect("An occupied square");
-        let letter = tile.letter().expect("A letter");
+    for (pos, tile) in word {
+        let letter = tile.letter().ok_or(GameError::MissingLetter)?;
 
         curr_state = fsm
             .traverse_from(curr_state, letter)
             .ok_or(GameError::InvalidWord)?;
 
-        let (tile_m, word_m) = match new.is_set(pos) {
+        let (tile_m, word_m) = match new[pos] {
             true => pos.premium_multipliers(),
             false => (1, 1),
         };
