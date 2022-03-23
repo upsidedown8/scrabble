2c3a16a4dbae3b9d5665b97e31d370bbb3702dc4
Tom Thorogood <tomthorogood@outlook.com>
2022-03-23 15:18:21 +0000

    Add `games` API types

--- api/src/games.rs
+++ api/src/games.rs
@@ -1,16 +1,43 @@
-// use uuid::Uuid;
+//! API types for live and past games.
 
-// pub struct GameCreate {
+use uuid::Uuid;
+use scrabble::game::{
+    play::Play,
+    PlayerId,
+};
 
-// }
+/// Messages sent during a live game.
+pub enum GameMessage {
+    /// Player took too long to make a move (sent to all players).
+    Timeout(PlayerId),
+    /// Player joined the game (sent to all players).
+    Joined(PlayerId),
+    /// Player sends a play to the server.
+    RequestPlay(Play),
+    /// There was an error making the play (invalid play or not the player's move).
+    PlayError(GameError),
+    /// A validated play was made (sent to all players). 
+    Play(Play),
+    /// Send a chat message.
+    SendChatMessage(String),
+    /// A message sent in live chat (sent to all players).
+    Chat(PlayerId, String),
+}
 
-// pub struct GameCreateResponse {
-//     pub game_id: Uuid,
-// }
+/// Messages sent in live chat.
+pub enum ChatMessage {
+    /// A string message.
+    String(String),
+}
 
-// pub enum GameMessage {
-//     Chat(PlayerId, Message),
-//     Join(PlayerDetails),
-//     Leave(PlayerId),
-//     Play(Play),
-// }
+/// Body of the request to create a game.
+pub struct CreateGame {
+    pub player_count: usize,
+    pub ai_count: usize,
+    pub timeout: Duration,
+}
+
+/// Response from creating a game.
+pub struct CreateGameResponse {
+    pub id_game: usize,
+}
