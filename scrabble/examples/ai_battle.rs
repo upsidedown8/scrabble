1cad6ce3d41c36fd75cf04950dbf7f8e87136e55
Tom Thorogood <tomthorogood@outlook.com>
2022-03-23 20:44:51 +0000

    Add 2 AI example

---
+++ scrabble/examples/ai_battle.rs
@@ -0,0 +1,34 @@
+use scrabble::{
+    ai::{highest_scoring::HighestScoring, Ai},
+    game::{Game, GameStatus},
+    util::fsm::FastFsm,
+};
+use std::{fs::File, io::BufReader};
+
+fn main() {
+    let file = File::open("../server/data/fast_fsm.bin").unwrap();
+    let rdr = BufReader::new(file);
+    let fsm: FastFsm = bincode::deserialize_from(rdr).unwrap();
+    let ai = HighestScoring::default();
+    let mut game = Game::with_players(2);
+
+    while let GameStatus::ToPlay(player_num) = game.status() {
+        let board = game.board();
+        let rack = game.player(*player_num).rack();
+        let play = ai.select_play(&fsm, board, rack, ());
+
+        if let Err(e) = game.make_play(&play, &fsm) {
+            eprintln!("err: {e}");
+            break;
+        }
+    }
+
+    println!("{}", game.board());
+    println!("{:#?}", game.status());
+
+    if let GameStatus::Over(game_over) = game.status() {
+        for (winner, score) in game_over.final_scores() {
+            println!("winner: {winner:?}; score: {score}");
+        }
+    }
+}
