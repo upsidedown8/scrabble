9d966da7f55f1dcb98993a56669f4e9e4a1f5c57
Tom Thorogood <tomthorogood@outlook.com>
2022-03-23 15:22:05 +0000

    Add `live_games` mod

--- server/src/live_games.rs
+++ server/src/live_games.rs
@@ -3,7 +3,7 @@ use std::{
     sync::{Arc, RwLock},
 };
 
-use scrabble::game::{Game, Player};
+use scrabble::game::{Game, PlayerId};
 use uuid::Uuid;
 use warp::ws::Ws;
 
@@ -12,9 +12,9 @@ pub struct LiveGames {
 }
 
 pub struct LiveGame {
-    players: HashMap<Player, PlayerType>,
+    id_game: Uuid,
+    players: HashMap<PlayerId, PlayerType>,
     game: Game,
-    id: Uuid,
 }
 
 pub enum PlayerType {
