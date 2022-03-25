e3fa8c6b48f0677c9539f37ba6a52ed967a92e28
Tom Thorogood <tomthorogood@outlook.com>
2022-03-25 18:52:11 +0000

    Update game api

--- api/src/games.rs
+++ api/src/games.rs
@@ -1,43 +1,43 @@
-// //! API types for live and past games.
+//! API types for live and past games.
 
-// use uuid::Uuid;
-// use scrabble::game::{
-//     play::Play,
-//     PlayerId,
-// };
+use uuid::Uuid;
+use scrabble::{error::GameError, game::{
+    play::Play,
+    PlayerNum,
+}};
 
-// /// Messages sent during a live game.
-// pub enum GameMessage {
-//     /// Player took too long to make a move (sent to all players).
-//     Timeout(PlayerId),
-//     /// Player joined the game (sent to all players).
-//     Joined(PlayerId),
-//     /// Player sends a play to the server.
-//     RequestPlay(Play),
-//     /// There was an error making the play (invalid play or not the player's move).
-//     PlayError(GameError),
-//     /// A validated play was made (sent to all players).
-//     Play(Play),
-//     /// Send a chat message.
-//     SendChatMessage(String),
-//     /// A message sent in live chat (sent to all players).
-//     Chat(PlayerId, String),
-// }
+/// Messages sent during a live game.
+pub enum GameMessage {
+    /// Player took too long to make a move (sent to all players).
+    Timeout(PlayerId),
+    /// Player joined the game (sent to all players).
+    Joined(PlayerId),
+    /// Player sends a play to the server.
+    RequestPlay(Play),
+    /// There was an error making the play (invalid play or not the player's move).
+    PlayError(GameError),
+    /// A validated play was made (sent to all players).
+    Play(Play),
+    /// Send a chat message.
+    SendChatMessage(ChatMessage),
+    /// A message sent in live chat (sent to all players).
+    Chat(PlayerId, ChatMessage),
+}
 
-// /// Messages sent in live chat.
-// pub enum ChatMessage {
-//     /// A string message.
-//     String(String),
-// }
+/// Messages sent in live chat.
+pub enum ChatMessage {
+    /// A string message.
+    String(String),
+}
 
-// /// Body of the request to create a game.
-// pub struct CreateGame {
-//     pub player_count: usize,
-//     pub ai_count: usize,
-//     pub timeout: Duration,
-// }
+/// Body of the request to create a game.
+pub struct CreateGame {
+    pub player_count: usize,
+    pub ai_count: usize,
+    pub timeout: Duration,
+}
 
-// /// Response from creating a game.
-// pub struct CreateGameResponse {
-//     pub id_game: usize,
-// }
+/// Response from creating a game.
+pub struct CreateGameResponse {
+    pub id_game: usize,
+}
