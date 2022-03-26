6e2e87cdac3a14d0e15b67131ecda735b248de03
Tom Thorogood <tomthorogood@outlook.com>
2022-03-26 17:20:58 +0000

    Update api `games` messages

--- api/src/games.rs
+++ api/src/games.rs
@@ -1,17 +1,23 @@
 //! API types for live and past games.
 
+use scrabble::{
+    error::GameError,
+    game::{play::Play, PlayerNum},
+};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use scrabble::{error::GameError, game::{
-    play::Play,
-    PlayerNum,
-}};
+
+use crate::auth::Auth;
 
 /// Messages sent during a live game.
+#[derive(Debug, Serialize, Deserialize)]
 pub enum GameMessage {
+    /// First message sent to the server, authenticates the user.
+    Authenticate(Auth),
     /// Player took too long to make a move (sent to all players).
-    Timeout(PlayerId),
+    Timeout(Uuid),
     /// Player joined the game (sent to all players).
-    Joined(PlayerId),
+    Joined(Uuid, PlayerNum),
     /// Player sends a play to the server.
     RequestPlay(Play),
     /// There was an error making the play (invalid play or not the player's move).
@@ -21,23 +27,25 @@ pub enum GameMessage {
     /// Send a chat message.
     SendChatMessage(ChatMessage),
     /// A message sent in live chat (sent to all players).
-    Chat(PlayerId, ChatMessage),
+    Chat(Uuid, ChatMessage),
 }
 
 /// Messages sent in live chat.
+#[derive(Debug, Serialize, Deserialize)]
 pub enum ChatMessage {
     /// A string message.
     String(String),
 }
 
 /// Body of the request to create a game.
+#[derive(Serialize, Deserialize)]
 pub struct CreateGame {
     pub player_count: usize,
     pub ai_count: usize,
-    pub timeout: Duration,
 }
 
 /// Response from creating a game.
+#[derive(Serialize, Deserialize)]
 pub struct CreateGameResponse {
     pub id_game: usize,
 }
