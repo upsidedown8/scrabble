8a91a376c12fde9c39edf971357870bb7f4793c2
Tom Thorogood <tomthorogood@outlook.com>
2022-03-27 20:18:59 +0100

    Update entry point

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,6 +1,9 @@
+/// The main entry point for the server.
 #[tokio::main]
-async fn main() {
+async fn main() -> server::error::Result<()> {
     env_logger::init();
     dotenv::dotenv().expect("`.env` file to be present");
-    server::serve(([127, 0, 0, 1], 8000)).await;
+    server::serve(([127, 0, 0, 1], 8000)).await?;
+
+    Ok(())
 }
