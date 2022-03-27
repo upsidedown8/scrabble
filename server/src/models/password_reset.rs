304f41cde91617b94b6c03afd008c82b4e2bea0e
Tom Thorogood <tomthorogood@outlook.com>
2022-03-27 15:17:59 +0100

    Implement `PasswordReset` model

--- server/src/models/password_reset.rs
+++ server/src/models/password_reset.rs
@@ -1,2 +1,14 @@
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+/// A record in `tbl_password_reset`.
 #[derive(Debug)]
-pub struct PasswordReset {}
+pub struct PasswordReset {
+    /// The id of the user who made the request.
+    pub id_user: Uuid,
+    /// A secret which is sent by email to verify the user's
+    /// password reset request.
+    pub secret_hex: String,
+    /// The time at which the `password_reset` is no longer valid.
+    pub valid_until: DateTime<Utc>,
+}
