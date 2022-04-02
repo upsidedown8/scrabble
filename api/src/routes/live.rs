71dfde205a1d04ada68fc0e9625f4a9983f83280
Tom Thorogood <tomthorogood@outlook.com>
2022-04-02 19:28:02 +0100

    SImplify message API for testing

--- api/src/routes/live.rs
+++ api/src/routes/live.rs
@@ -1,54 +1,24 @@
 //! API types for live games.
 
 use crate::auth::Auth;
-use scrabble::game::{play::Play, PlayerNum};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-/// Info about a player.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Player {
-    /// Id of the player.
-    pub id_player: i32,
-    /// Username of the player.
-    pub username: String,
-}
-
-/// The first message sent, authenticates the user.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AuthMsg(pub Auth);
 
 /// Messages sent from the client.
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ClientMsg {
+    /// The first message sent, authenticates the user.
+    Auth(Auth),
+    /// Request to join a game.
+    Join(i32),
+    /// Request to create a game with a fixed number of players.
+    Create(usize),
     /// A chat message.
     Chat(String),
-    /// Attempt to make a play.
-    Play(Play),
 }
 
 /// Messages sent from the server.
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ServerMsg {
-    /// A player has left or joined the game.
-    Players(HashMap<PlayerNum, Player>),
     /// A chat message.
-    Chat(Player, String),
-    /// A player has made a play.
-    Play(Play),
-    /// Sent to a user that sent an illegal play.
-    IllegalPlay,
-    /// The server state has updated.
-    State(ServerState),
-}
-
-/// The state of the live game.
-#[derive(Debug, Serialize, Deserialize)]
-pub enum ServerState {
-    /// The server is waiting for players.
-    Joining { needed: usize },
-    /// It is a player's turn.
-    ToPlay(Player),
-    /// The game is over.
-    Over,
+    Chat(i32, String),
 }
