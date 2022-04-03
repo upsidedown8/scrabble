48dba8c7ac2e7ed365ef4bd0ab1e7631107eac40
Tom Thorogood <tomthorogood@outlook.com>
2022-04-03 16:29:00 +0100

    Rename `rooms` to `games`

--- server/src/filters/live.rs
+++ server/src/filters/live.rs
@@ -2,26 +2,24 @@ use crate::{
     db::Db,
     filters::with,
     fsm::FsmHandle,
-    handlers::{self, live::rooms::RoomsHandle},
+    handlers::{self, live::games::GamesHandle},
 };
 use warp::{filters::BoxedFilter, ws::Ws, Filter, Reply};
 
 /// Combined filter for the live route.
 pub fn all(db: &Db, fsm: &FsmHandle) -> BoxedFilter<(impl Reply,)> {
-    let rooms = &RoomsHandle::default();
+    let games = GamesHandle::new(db, fsm);
 
-    connect(db, fsm, rooms).boxed()
+    connect(&games)
 }
 
 /// Connect to the server via websocket.
-fn connect(db: &Db, fsm: &FsmHandle, rooms: &RoomsHandle) -> BoxedFilter<(impl Reply,)> {
+fn connect(games: &GamesHandle) -> BoxedFilter<(impl Reply,)> {
     warp::path!("api" / "live")
         .and(warp::ws())
-        .and(with(db))
-        .and(with(fsm))
-        .and(with(rooms))
-        .map(|ws: Ws, db: Db, fsm: FsmHandle, rooms: RoomsHandle| {
-            ws.on_upgrade(move |ws| handlers::live::connect(ws, db, fsm, rooms));
+        .and(with(games))
+        .map(|ws: Ws, games: GamesHandle| {
+            ws.on_upgrade(move |ws| handlers::live::connected(ws, games));
             warp::reply()
         })
         .boxed()
