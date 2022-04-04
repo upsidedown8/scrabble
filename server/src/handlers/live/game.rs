675fb8e89e2ea8233e074c435694ec5b112aae57
Tom Thorogood <tomthorogood@outlook.com>
2022-04-04 19:13:29 +0100

    Implement game creation

--- server/src/handlers/live/game.rs
+++ server/src/handlers/live/game.rs
@@ -1,4 +1,8 @@
-use crate::{db::Db, fsm::FsmHandle, models::AiDifficulty};
+use crate::{
+    db::Db,
+    fsm::FsmHandle,
+    models::{self, AiDifficulty},
+};
 use api::routes::live::{ClientMsg, LiveError, Player, ServerMsg};
 use scrabble::{
     ai::Ai,
@@ -28,6 +32,74 @@ impl Deref for GameHandle {
         self.0.deref()
     }
 }
+impl GameHandle {
+    /// Creates a new [`GameHandle`] from the player count, returning
+    /// the id of the game.
+    pub async fn create(
+        db: Db,
+        fsm: FsmHandle,
+        ai_count: usize,
+        player_count: usize,
+        id_owner: Option<i32>,
+    ) -> Option<(i32, GameHandle)> {
+        let total_count = ai_count + player_count;
+        // create a queue that allows connected clients to send messages
+        // to the game (multiple producers) and the game to receive the
+        // message (single consumer).
+        let (sender, mut receiver) = mpsc::unbounded_channel::<GameMsg>();
+        // create a database record for the game.
+        let id_game = models::Game::insert(&db).await.ok()?;
+        // create database records for the ai players.
+        let mut slots = HashMap::default();
+        for player_num in PlayerNum::iter(total_count).take(ai_count) {
+            let difficulty = AiDifficulty::Medium;
+
+            // insert a record for each ai player.
+            let id_player = models::Player::insert_ai(&db, id_game, difficulty)
+                .await
+                .ok()?;
+
+            // insert into slots hashmap.
+            slots.insert(
+                player_num,
+                Slot {
+                    id_player,
+                    game_player: GamePlayer::Ai { difficulty },
+                },
+            );
+        }
+
+        // create the game.
+        let game = Game {
+            game: scrabble::game::Game::new(total_count),
+            play_count: 0,
+            slots,
+            db,
+            fsm,
+            id_game,
+            id_owner,
+            sender,
+        };
+        let game_handle = GameHandle(Arc::new(Mutex::new(game)));
+
+        // spawn a task to listen for messages and act on them.
+        tokio::spawn({
+            let game_handle = game_handle.clone();
+            async move {
+                while let Some(msg) = receiver.recv().await {
+                    log::debug!("game message received: {msg:?}");
+                    // lock the game and handle the message.
+                    let mut game_lock = game_handle.lock().await;
+                    game_lock.on_msg(msg, game_handle.clone()).await;
+                }
+
+                log::info!("closing game: {id_game}");
+            }
+        });
+
+        Some((id_game, game_handle))
+    }
+}
 
 /// Handles live games.
 #[derive(Debug)]
@@ -53,9 +125,13 @@ impl Game {
     pub fn sender(&self) -> mpsc::UnboundedSender<GameMsg> {
         self.sender.clone()
     }
-
+    /// Attempts to add a player to the game. Return value indicates
+    /// success.
+    pub async fn add_player(&mut self) -> bool {
+        todo!()
+    }
     /// Called when a message is received from a user.
-    pub async fn on_msg(&mut self, msg: GameMsg, game_handle: GameHandle) {
+    async fn on_msg(&mut self, msg: GameMsg, game_handle: GameHandle) {
         let GameMsg { msg, id_user } = msg;
 
         match msg {
@@ -107,7 +183,7 @@ impl Game {
         }
 
         // attempt to make the play.
-        if self.try_play(play, player_num) {
+        if self.try_play(play, player_num).await {
             // make plays for any ai players.
             self.make_ai_plays();
 
@@ -120,21 +196,20 @@ impl Game {
     }
     /// Continues to make plays for Ai players until a connected
     /// player is encountered or the game ends.
-    fn make_ai_plays(&mut self) {
-        let fsm: &FastFsm = &self.fsm;
-
+    async fn make_ai_plays(&mut self) {
         // loop until game is over.
         while let Some(to_play) = self.game.to_play() {
             if let Some(ai) = &self.slots[&to_play].ai() {
+                let fsm: &FastFsm = &self.fsm;
                 let play = ai.next_play(fsm, &self.game);
-                let is_success = self.try_play(play, to_play);
+                let is_success = self.try_play(play, to_play).await;
 
                 assert!(is_success, "Ai move should always be valid");
             }
         }
     }
     /// Attempts to make a play. Return value indicates success.
-    fn try_play(&mut self, play: Play, player_num: PlayerNum) -> bool {
+    async fn try_play(&mut self, play: Play, player_num: PlayerNum) -> bool {
         // store the current tile positions.
         let prev_tiles = self.api_tiles();
         let fsm: &FastFsm = &self.fsm;
