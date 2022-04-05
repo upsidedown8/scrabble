fc28fe7d6a262af587c8cc98e86057d978925557
Tom Thorogood <tomthorogood@outlook.com>
2022-04-05 21:38:56 +0100

    Impl leaderboard pages

--- client/src/routes/leaderboards/leaderboard.rs
+++ client/src/routes/leaderboards/leaderboard.rs
@@ -1,11 +1,37 @@
 //! Implementation of the [`LeaderboardPage`].
 
-use sycamore::prelude::*;
+use crate::{
+    components::{ErrorMsg, Leaderboard},
+    services::leaderboard::overall_leaderboard,
+};
+use api::routes::leaderboard::LeaderboardResponse;
+use sycamore::{futures::ScopeSpawnLocal, prelude::*};
 
 /// Page for the overall leaderboard.
 #[component]
 pub fn LeaderboardPage<G: Html>(ctx: ScopeRef) -> View<G> {
+    // State signals
+    let leaderboard = ctx.create_signal(vec![]);
+    let err = ctx.create_signal(None);
+
+    // Attempt to load the leaderboard.
+    ctx.spawn_local(async {
+        match overall_leaderboard(15, 0).await {
+            Ok(LeaderboardResponse { rows }) => leaderboard.set(rows),
+            Err(e) => err.set(Some(e)),
+        }
+    });
+
     view! { ctx,
-        p { "TODO" }
+        div(class="page is-centered") {
+            div {
+                div(class="m-3 has-text-centered") {
+                    h1(class="h1 is-size-5") { "Leaderboard" }
+                }
+
+                Leaderboard(leaderboard)
+                ErrorMsg(err)
+            }
+        }
     }
 }
