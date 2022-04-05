fd41c0b3a3e0988164bc619124c0e7941cd4e69a
Tom Thorogood <tomthorogood@outlook.com>
2022-04-05 17:58:18 +0100

    Update reset link format

--- server/src/handlers/users.rs
+++ server/src/handlers/users.rs
@@ -60,7 +60,7 @@ pub async fn reset_password(
 
     // send a reset password email.
     let reset_link = format!(
-        "https://scrabble.thrgd.uk/reset-password&secret={hex}&username={username}",
+        "https://scrabble.thrgd.uk/reset-password/{username}/{hex}",
         username = user.username,
         hex = secret_hex
     );
