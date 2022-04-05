0f660eaea28a324d41c531aaa825eda000c75d56
Tom Thorogood <tomthorogood@outlook.com>
2022-04-05 20:13:05 +0100

    Implement ResetPasswordPage

--- client/src/routes/reset_password.rs
+++ client/src/routes/reset_password.rs
@@ -1,27 +1,70 @@
 //! Page that allows users to send an email to reset their password.
 
-use sycamore::prelude::*;
+use crate::{
+    components::{ErrorMsg, ProgressBar},
+    services::users::reset_password,
+};
+use api::routes::users::ResetPassword;
+use sycamore::{futures::ScopeSpawnLocal, prelude::*};
 
 /// ResetPassword page.
 #[component]
 pub fn ResetPasswordPage<G: Html>(ctx: ScopeRef) -> View<G> {
-    let password = ctx.create_signal(String::new());
+    // Input signal
+    let username = ctx.create_signal(String::new());
+
+    // State signals
+    let loading = ctx.create_signal(false);
+    let err = ctx.create_signal(None);
+    let success = ctx.create_signal(false);
+
+    // Called when the user clicks the reset button.
+    let on_reset = |_| {
+        loading.set(true);
+        err.set(None);
+
+        ctx.spawn_local(async {
+            let req = ResetPassword {
+                username: (*username.get()).clone(),
+            };
+
+            match reset_password(&req).await {
+                Ok(()) => {}
+                Err(e) => err.set(Some(e)),
+            }
+
+            loading.set(false);
+        })
+    };
 
     view! { ctx,
         div(class="page is-centered is-vcentered is-flex columns") {
             div(class="box") {
-                h1 { "Reset password" }
-
-                hr
-
                 div(class="field") {
                     label(class="label") {
-                        "New password"
+                        "Username"
                     }
                     div(class="control") {
-                        input(type="password", class="input", placeholder="**********", bind:value=password)
+                        input(type="text", class="input", placeholder="Username", bind:value=username)
                     }
                 }
+
+                button(on:click=on_reset, disabled=*loading.get(), class="button is-primary") {
+                    "Reset password"
+                }
+
+                ProgressBar(loading)
+                ErrorMsg(err)
+
+                (if *success.get() {
+                    view! { ctx,
+                        p {
+                            "Request succeeded. Check your email."
+                        }
+                    }
+                } else {
+                    view! { ctx, }
+                })
             }
         }
     }
