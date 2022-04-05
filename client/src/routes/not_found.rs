b8036b5c0e1e35ec5604448222c2241660c4ab71
Tom Thorogood <tomthorogood@outlook.com>
2022-04-05 19:02:41 +0100

    Use `page` css class for `NotFoundPage`

--- client/src/routes/not_found.rs
+++ client/src/routes/not_found.rs
@@ -7,7 +7,7 @@ use sycamore::prelude::*;
 #[component]
 pub fn NotFoundPage<G: Html>(ctx: ScopeRef) -> View<G> {
     view! { ctx,
-        div(class="not-found-route is-centered is-vcentered columns") {
+        div(class="page is-centered is-vcentered columns") {
             div {
                 h1 {
                     "404: Not Found"
