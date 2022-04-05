262f8a205e616bdf8d0e3a6c7498b2c8f3b658f3
Tom Thorogood <tomthorogood@outlook.com>
2022-04-05 20:19:29 +0100

    Implement ResetWithSecret

--- client/src/routes/reset_with_secret.rs
+++ client/src/routes/reset_with_secret.rs
@@ -1,6 +1,13 @@
 //! Page that allows users to update their password.
 
-use sycamore::prelude::*;
+use crate::{
+    components::{ErrorMsg, ProgressBar},
+    contexts::{AuthCtx, ScopeExt},
+    services::users::reset_with_secret,
+};
+use api::routes::users::ResetWithSecret;
+use sycamore::{futures::ScopeSpawnLocal, prelude::*};
+use sycamore_router::navigate;
 
 #[derive(Prop)]
 pub struct Props {
@@ -11,7 +18,39 @@ pub struct Props {
 /// ResetWithSecret page.
 #[component]
 pub fn ResetWithSecretPage<G: Html>(ctx: ScopeRef, props: Props) -> View<G> {
+    // Input signal
     let password = ctx.create_signal(String::new());
+    let props = ctx.create_signal(props);
+
+    // State signals
+    let loading = ctx.create_signal(false);
+    let err = ctx.create_signal(None);
+
+    // Called when the user resets their password
+    let on_reset = |_| {
+        loading.set(true);
+        err.set(None);
+
+        ctx.spawn_local(async {
+            let req = ResetWithSecret {
+                secret_hex: props.get().secret.clone(),
+                new_password: (*password.get()).clone(),
+                username: props.get().username.clone(),
+            };
+
+            match reset_with_secret(&req).await {
+                Ok((auth, user_details)) => {
+                    // Update the token and navigate to the homepage.
+                    ctx.use_auth_context()
+                        .set(Some(AuthCtx { user_details, auth }));
+                    navigate("/");
+                }
+                Err(e) => err.set(Some(e)),
+            }
+
+            loading.set(false);
+        });
+    };
 
     view! { ctx,
         div(class="page is-centered is-vcentered is-flex columns") {
@@ -28,6 +67,13 @@ pub fn ResetWithSecretPage<G: Html>(ctx: ScopeRef, props: Props) -> View<G> {
                         input(type="password", class="input", placeholder="**********", bind:value=password)
                     }
                 }
+
+                button(on:click=on_reset, disabled=*loading.get(), class="button is-primary") {
+                    "Update password"
+                }
+
+                ProgressBar(loading)
+                ErrorMsg(err)
             }
         }
     }
