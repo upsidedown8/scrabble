e7d27e4eba0da8fa2ff637c9c60ed80b25a38a4d
Tom Thorogood <tomthorogood@outlook.com>
2022-04-05 18:44:48 +0100

    Implement all services

--- client/src/services/mod.rs
+++ client/src/services/mod.rs
@@ -8,47 +8,21 @@ use api::auth::{Auth, AuthWrapper};
 use reqwasm::http::{Method, Request};
 use serde::{de::DeserializeOwned, Serialize};
 
+pub mod friends;
+pub mod games;
+pub mod leaderboard;
 pub mod users;
 
 const API_URL: &str = "https://thrgd.uk/api";
 
-/// Make a request to the path {API_URL}/{url}, with the provided
-/// method and data. Returns the optional auth from the server and
-/// the requested value.
-pub async fn request<T, U>(
-    auth: Option<&Auth>,
-    url: &str,
-    data: &T,
-    method: Method,
-) -> Result<(Option<Auth>, U)>
+/// Sends the [`Request`].
+async fn send_request<U>(req: Request) -> Result<(Option<Auth>, U)>
 where
-    T: Serialize,
     U: DeserializeOwned,
 {
-    let request_url = format!("{API_URL}{url}");
-
-    let body = serde_json::to_string(data)?;
-
-    log::info!("method: {method}, url: {request_url}, body: {body}");
-
-    let mut req = Request::new(&request_url)
-        .method(method)
-        .header("Content-Type", "Application/JSON")
-        .body(body);
-
-    log::info!("{req:#?}");
-
-    if let Some(Auth(token)) = auth {
-        let bearer = format!("Bearer {token}");
-
-        req = req.header("Authorization", &bearer);
-
-        log::info!("added auth header");
-    }
-
     let response = req.send().await?;
 
-    log::info!("{response:?}");
+    log::info!("response received: {response:?}");
 
     // match on the response http status and return either
     // an error message or the deserialized content.
@@ -76,7 +50,69 @@ where
     }
 }
 
-/// Makes a request and update the auth field if a token is
+/// Make a request to the path {API_URL}/{url}, with the provided
+/// method and data. Returns the optional auth from the server and
+/// the requested value.
+pub async fn request<T, U>(
+    auth: Option<&Auth>,
+    url: &str,
+    data: &T,
+    method: Method,
+) -> Result<(Option<Auth>, U)>
+where
+    T: Serialize,
+    U: DeserializeOwned,
+{
+    let request_url = format!("{API_URL}{url}");
+    let body = serde_json::to_string(data)?;
+
+    log::info!("method: {method}, url: {request_url}, body: {body}");
+
+    let mut req = Request::new(&request_url)
+        .method(method)
+        .header("Content-Type", "Application/JSON")
+        .body(body);
+
+    log::info!("{req:#?}");
+
+    if let Some(Auth(token)) = auth {
+        let bearer = format!("Bearer {token}");
+
+        req = req.header("Authorization", &bearer);
+
+        log::info!("added auth header");
+    }
+
+    send_request(req).await
+}
+
+/// Makes a request with no body.
+pub async fn request_no_body<U>(
+    auth: Option<&Auth>,
+    url: &str,
+    method: Method,
+) -> Result<(Option<Auth>, U)>
+where
+    U: DeserializeOwned,
+{
+    let request_url = format!("{API_URL}{url}");
+
+    log::info!("method: {method}, url: {request_url}, empty body");
+
+    let mut req = Request::new(&request_url).method(method);
+
+    if let Some(Auth(token)) = auth {
+        let bearer = format!("Bearer {token}");
+
+        req = req.header("Authorization", &bearer);
+
+        log::info!("added auth header");
+    }
+
+    send_request(req).await
+}
+
+/// Makes a request and updates the auth field if a token is
 /// received.
 pub async fn std_request<T, U>(
     auth_ctx: &AuthSignal,
@@ -94,7 +130,32 @@ where
 
     // Make the request.
     let (auth, response) = request(token, url, data, method).await?;
+    update_auth(auth_ctx, auth);
 
+    // Return the response from the server.
+    Ok(response)
+}
+
+/// Makes a request with no body and updates the auth field
+/// if a token is received.
+pub async fn std_request_no_body<U>(auth_ctx: &AuthSignal, url: &str, method: Method) -> Result<U>
+where
+    U: DeserializeOwned,
+{
+    // Extract the token from the auth signal.
+    let ctx = auth_ctx.get();
+    let token = ctx.as_ref().as_ref().map(|ctx| &ctx.auth);
+
+    // Make the request.
+    let (auth, response) = request_no_body(token, url, method).await?;
+    update_auth(auth_ctx, auth);
+
+    // Return the response from the server.
+    Ok(response)
+}
+
+/// Updates the global auth state if `auth` is `Some`.
+fn update_auth(auth_ctx: &AuthSignal, auth: Option<Auth>) {
     // If a new auth token is received, update the stored token.
     if let Some(auth) = auth {
         let details = auth_ctx
@@ -108,7 +169,4 @@ where
             auth,
         }))
     }
-
-    // Return the response from the server.
-    Ok(response)
 }
--- client/src/services/users.rs
+++ client/src/services/users.rs
@@ -2,33 +2,49 @@
 
 use crate::{
     contexts::AuthSignal,
-    error::Error,
-    services::{request, std_request},
+    error::Result,
+    services::{request, std_request, std_request_no_body},
 };
 use api::{auth::Auth, routes::users::*};
 use reqwasm::http::Method;
 
 /// POST /api/users/login
-pub async fn login(req: &Login) -> Result<(Option<Auth>, LoginResponse), Error> {
-    request(None, "/users/login", req, Method::POST).await
+pub async fn login(req: &Login) -> Result<(Auth, UserDetails)> {
+    let (auth, user_details) = request(None, "/users/login", req, Method::POST).await?;
+    Ok((auth.unwrap(), user_details))
 }
 
 /// POST /api/users
-pub async fn sign_up(req: &SignUp) -> Result<(Option<Auth>, SignUpResponse), Error> {
-    request(None, "/users", req, Method::POST).await
+pub async fn sign_up(req: &SignUp) -> Result<(Auth, UserDetails)> {
+    let (auth, user_details) = request(None, "/users", req, Method::POST).await?;
+    Ok((auth.unwrap(), user_details))
 }
 
 /// GET /api/users [+Auth]
-pub async fn profile(auth_ctx: &AuthSignal) -> Result<ProfileResponse, Error> {
-    std_request(auth_ctx, "/users", &(), Method::GET).await
+pub async fn profile(auth_ctx: &AuthSignal) -> Result<UserDetails> {
+    std_request_no_body(auth_ctx, "/users", Method::GET).await
 }
 
 /// PUT /api/users [+Auth]
-pub async fn update(auth_ctx: &AuthSignal, req: &UpdateAccount) -> Result<(), Error> {
+pub async fn update(auth_ctx: &AuthSignal, req: &UpdateAccount) -> Result<()> {
     std_request(auth_ctx, "/users", req, Method::PUT).await
 }
 
 /// DELETE /api/users [+Auth]
-pub async fn delete(auth_ctx: &AuthSignal, req: &DeleteAccount) -> Result<(), Error> {
+pub async fn delete(auth_ctx: &AuthSignal, req: &DeleteAccount) -> Result<()> {
     std_request(auth_ctx, "/users", req, Method::DELETE).await
+}
+
+/// POST /api/users/reset-password
+pub async fn reset_password(req: &ResetPassword) -> Result<()> {
+    let (_, ()) = request(None, "/users/reset-password", req, Method::POST).await?;
+
+    // no value is returned from this route.
+    Ok(())
+}
+
+/// GET /api/users/reset-password
+pub async fn reset_with_secret(req: &ResetWithSecret) -> Result<(Auth, UserDetails)> {
+    let (auth, user_details) = request(None, "/users/reset-password", req, Method::GET).await?;
+    Ok((auth.unwrap(), user_details))
 }
