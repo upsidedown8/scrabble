9d347bf2442e6c6558fb9490a8f362ebaf55277c
Tom Thorogood <tomthorogood@outlook.com>
2022-04-06 00:23:09 +0100

    Set up friends route

--- client/src/routes/friends.rs
+++ client/src/routes/friends.rs
@@ -1,11 +1,22 @@
 //! Implementation of the [`FriendsPage`].
 
+use crate::components::ErrorMsg;
 use sycamore::prelude::*;
 
 /// Page for managing friends and friend requests.
 #[component]
 pub fn FriendsPage<G: Html>(ctx: ScopeRef) -> View<G> {
+    let err = ctx.create_signal(None);
+
     view! { ctx,
-        p { "TODO" }
+        div(class="page is-centered") {
+            div {
+                div(class="m-3 has-text-centered") {
+                    h1(class="h1 is-size-5") { "Friends" }
+                }
+
+                ErrorMsg(err)
+            }
+        }
     }
 }
