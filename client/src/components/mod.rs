f518e9b480cd983b2453095f3bac9e534247558b
Tom Thorogood <tomthorogood@outlook.com>
2022-04-12 22:26:08 +0100

    Revert "Implement `Board`, `Tile`, `Rack` components"
    This reverts commit b66834153a38f25c27b9f72e734b4b1f4ce3fe69.

--- client/src/components/mod.rs
+++ client/src/components/mod.rs
@@ -1,6 +1,5 @@
 //! Reuseable components used across pages.
 
-mod board;
 mod error_msg;
 mod fa_icon;
 mod footer;
@@ -8,11 +7,8 @@ mod friends_table;
 mod leaderboard;
 mod navbar;
 mod progress;
-mod rack;
 mod separator;
-mod tile;
 
-pub use board::Board;
 pub use error_msg::{ErrorMsg, StaticErrorMsg};
 pub use fa_icon::FaIcon;
 pub use footer::Footer;
@@ -20,6 +16,4 @@ pub use friends_table::FriendsTable;
 pub use leaderboard::Leaderboard;
 pub use navbar::Navbar;
 pub use progress::Progress;
-pub use rack::Rack;
 pub use separator::Separator;
-pub use tile::Tile;
