f98438759e7d48988de08cb5e764cebd4599795f
Tom Thorogood <tomthorogood@outlook.com>
2022-04-12 14:18:23 +0100

    Centre the leaderboard

--- client/src/components/leaderboard.rs
+++ client/src/components/leaderboard.rs
@@ -14,36 +14,38 @@ pub struct Props<'a> {
 #[component]
 pub fn Leaderboard<'a, G: Html>(cx: Scope<'a>, props: Props<'a>) -> View<G> {
     view! { cx,
-        table(class="table") {
-            // define the headers of the table.
-            thead {
-                tr {
-                    th {abbr(title="Username") { "id" }}
-                    th {abbr(title="Average score") { "score/play" }}
-                    th {abbr(title="Average word length") { "wlen/play" }}
-                    th {abbr(title="Average tiles placed") { "tiles/play" }}
-                    th {abbr(title="Longest word length") { "longest" }}
-                    th {abbr(title="Best score") { "best" }}
-                    th {abbr(title="Average score per game") { "score/game" }}
-                    th {abbr(title="Average score per tile") { "score/tile" }}
-                    th {abbr(title="Win percentage") { "w%" }}
+        div(class="columns is-centered") {
+            table(class="table") {
+                // define the headers of the table.
+                thead {
+                    tr {
+                        th {abbr(title="Username") { "id" }}
+                        th {abbr(title="Average score") { "score/play" }}
+                        th {abbr(title="Average word length") { "wlen/play" }}
+                        th {abbr(title="Average tiles placed") { "tiles/play" }}
+                        th {abbr(title="Longest word length") { "longest" }}
+                        th {abbr(title="Best score") { "best" }}
+                        th {abbr(title="Average score per game") { "score/game" }}
+                        th {abbr(title="Average score per tile") { "score/tile" }}
+                        th {abbr(title="Win percentage") { "w%" }}
+                    }
                 }
-            }
-            // define the body of the table.
-            tbody {
-                Indexed {
-                    iterable: props.rows,
-                    view: |cx, row| view! { cx,
-                        tr {
-                            td { (row.username) }
-                            td { (row.avg_score_per_play) }
-                            td { (row.avg_word_length) }
-                            td { (row.avg_tiles_per_play) }
-                            td { (row.longest_word_length) }
-                            td { (row.best_word_score) }
-                            td { (row.avg_score_per_game) }
-                            td { (row.avg_score_per_tile) }
-                            td { (row.win_percentage) }
+                // define the body of the table.
+                tbody {
+                    Indexed {
+                        iterable: props.rows,
+                        view: |cx, row| view! { cx,
+                            tr {
+                                td { (row.username) }
+                                td { (row.avg_score_per_play) }
+                                td { (row.avg_word_length) }
+                                td { (row.avg_tiles_per_play) }
+                                td { (row.longest_word_length) }
+                                td { (row.best_word_score) }
+                                td { (row.avg_score_per_game) }
+                                td { (row.avg_score_per_tile) }
+                                td { (row.win_percentage) }
+                            }
                         }
                     }
                 }
