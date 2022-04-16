9667a3ff3675decbe53e1e707ef87db44f5ed012
Tom Thorogood <tomthorogood@outlook.com>
2022-04-16 18:10:40 +0100

    Implement basic controls

--- client/src/pages/live/playing.rs
+++ client/src/pages/live/playing.rs
@@ -4,6 +4,7 @@ use crate::{
 };
 use api::routes::live::ClientMsg;
 use futures::{channel::mpsc, SinkExt};
+use scrabble::game::play::Play;
 use sycamore::{
     futures::{spawn_local, spawn_local_scoped},
     prelude::*,
@@ -25,38 +26,52 @@ pub struct Props<'a> {
 pub fn Playing<'a, G: Html>(cx: Scope<'a>, props: Props<'a>) -> View<G> {
     let state = create_memo(cx, || match props.state.get().as_ref() {
         AppState::Playing(playing_state) => playing_state.clone(),
-        AppState::Connected => unreachable!(),
+        AppState::Connected(..) => unreachable!(),
     });
 
-    let rack = state.get().local_rack.clone();
-    let tiles = state.get().local_tiles.clone();
+    let rack = create_memo(cx, || {
+        let state = state.get();
+        let state = state.as_ref().as_ref();
+        state.rack.clone()
+    });
+    let tiles = create_memo(cx, || {
+        let state = state.get();
+        let state = state.as_ref().as_ref();
+        state.tiles.clone()
+    });
     let messages = state.get().messages.clone();
     let scores = create_memo(cx, || state.get().scores.clone());
+    let show_premiums = create_signal(cx, false);
 
-    // called when a board square is clicked.
-    let on_click_square = {
+    let send_app_msg = {
         let dispatch_write = props.dispatch_write.clone();
-        move |pos| {
+        move |msg| {
             let mut dispatch_write = dispatch_write.clone();
             spawn_local(async move {
-                dispatch_write
-                    .send(AppMsg::SquareClicked(pos))
-                    .await
-                    .unwrap();
+                dispatch_write.send(msg).await.unwrap();
             });
         }
     };
 
+    // called when a board square is clicked.
+    let on_click_square = {
+        let send_msg = send_app_msg.clone();
+        move |pos| send_msg(AppMsg::SquareClicked(pos))
+    };
+
     // called when a rack tile is clicked.
     let on_click_rack_tile = {
-        let dispatch_write = props.dispatch_write.clone();
-        move |tile| {
-            let mut dispatch_write = dispatch_write.clone();
-            spawn_local(async move {
-                dispatch_write
-                    .send(AppMsg::RackTileClicked(tile))
-                    .await
-                    .unwrap();
+        let send_msg = send_app_msg;
+        move |tile| send_msg(AppMsg::RackTileClicked(tile))
+    };
+
+    // called when the pass button is clicked.
+    let on_pass = {
+        let ws_write = props.ws_write.clone();
+        move |_| {
+            let mut ws_write = ws_write.clone();
+            spawn_local_scoped(cx, async move {
+                ws_write.send(ClientMsg::Play(Play::Pass)).await.unwrap();
             });
         }
     };
@@ -72,6 +87,7 @@ pub fn Playing<'a, G: Html>(cx: Scope<'a>, props: Props<'a>) -> View<G> {
     view! { cx,
         div(class="live") {
             Board {
+                show_premiums: show_premiums,
                 on_click: on_click_square,
                 cells: tiles,
             }
@@ -81,9 +97,19 @@ pub fn Playing<'a, G: Html>(cx: Scope<'a>, props: Props<'a>) -> View<G> {
                 tiles: rack,
             }
 
-            // Controls {
-
-            // }
+            div(class="controls") {
+                div(class="buttons") {
+                    button(class="button is-dark") {
+                        "Redraw"
+                    }
+                    button(class="button is-dark") {
+                        "Place"
+                    }
+                    button(class="button is-dark", on:click=on_pass) {
+                        "Pass"
+                    }
+                }
+            }
 
             Scoreboard {
                 scores: scores,
