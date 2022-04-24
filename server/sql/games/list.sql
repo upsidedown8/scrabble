de9270bb196095486694418d5b14a5b1e39cd9e0
Tom Thorogood <tomthorogood@outlook.com>
2022-04-24 12:25:11 +0100

    Order game list by id

--- server/sql/games/list.sql
+++ server/sql/games/list.sql
@@ -5,4 +5,5 @@ SELECT tbl_game.id_game AS id_game,
 FROM tbl_player
     JOIN tbl_human_player ON tbl_human_player.id_player = tbl_player.id_player
     JOIN tbl_game ON tbl_player.id_game = tbl_game.id_game
-WHERE tbl_human_player.id_user = $1;
+WHERE tbl_human_player.id_user = $1
+ORDER BY tbl_game.id_game DESC;
